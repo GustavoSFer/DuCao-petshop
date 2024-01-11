d47863b7278b310034d5d10bffe3aa7f5dfb5c12
GustavoSFer <gustavo.sfernander@gmail.com>
2024-01-10 21:09:03 -0300

    Iniciando o projeto

---
+++ src/main/java/com/fernandes/ecommercelovepet/DuCaoPetshopApplication.java
@@ -0,0 +1,13 @@
+package com.fernandes.ecommercelovepet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuCaoPetshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuCaoPetshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandes/ecommercelovepet/DuCaoPetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandes.ecommercelovepet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuCaoPetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
