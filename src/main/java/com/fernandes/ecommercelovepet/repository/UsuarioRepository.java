6cbb4d650de43a9b077538b347c6dfa75b752419
GustavoSFer <gustavo.sfernander@gmail.com>
2024-01-12 19:12:38 -0300

    Feat: Criando o repository de usuario para a interação com o banco de dados

---
+++ src/main/java/com/fernandes/ecommercelovepet/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.fernandes.ecommercelovepet.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fernandes.ecommercelovepet.entities.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+
+}
