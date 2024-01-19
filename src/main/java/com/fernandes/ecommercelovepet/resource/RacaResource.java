d77664be92e9238d40ba0b41b636238dc27155f6
GustavoSFer <gustavo.sfernander@gmail.com>
2024-01-19 19:49:47 -0300

    Feat: Buscando uma raca especifica

--- src/main/java/com/fernandes/ecommercelovepet/resource/RacaResource.java
+++ src/main/java/com/fernandes/ecommercelovepet/resource/RacaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,12 @@ public class RacaResource {
 		List<Raca> racas = racaService.findAll();
 		
 		return ResponseEntity.ok().body(racas);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Raca> findById(@PathVariable Integer id) {
+		Raca raca = racaService.findById(id);
+		
+		return ResponseEntity.ok().body(raca);
 	}
 }
--- src/main/java/com/fernandes/ecommercelovepet/service/RacaService.java
+++ src/main/java/com/fernandes/ecommercelovepet/service/RacaService.java
@@ -1,25 +1,32 @@
 package com.fernandes.ecommercelovepet.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fernandes.ecommercelovepet.entities.Raca;
 import com.fernandes.ecommercelovepet.repository.RacaRepository;
+import com.fernandes.ecommercelovepet.service.exception.ResourceNotFound;
 
 @Service
 public class RacaService {
 	
 	@Autowired
-	private RacaRepository raca;
+	private RacaRepository racaService;
 	
 	
 	public List<Raca> findAll() {
-		List<Raca> racas = raca.findAll();
+		List<Raca> racas = racaService.findAll();
 		
 		return racas;
 	}
 	
+	public Raca findById(Integer id) {
+		Optional<Raca> raca = racaService.findById(id);
+		
+		return raca.orElseThrow(() -> new ResourceNotFound());
+	}
 
 }
