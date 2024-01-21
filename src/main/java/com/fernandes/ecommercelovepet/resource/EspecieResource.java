0ebfa1a94941ea0933ab4fc3c2966556b9f1541e
GustavoSFer <gustavo.sfernander@gmail.com>
2024-01-21 20:16:13 -0300

    Feat: Buscando uma especie especifica

--- src/main/java/com/fernandes/ecommercelovepet/resource/EspecieResource.java
+++ src/main/java/com/fernandes/ecommercelovepet/resource/EspecieResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,13 @@ public class EspecieResource {
 		List<Especie> especies = especieService.findAll();
 		
 		return ResponseEntity.ok().body(especies);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Especie> findById(@PathVariable Integer id) {
+		Especie especie = especieService.findById(id);
+		
+		return ResponseEntity.ok().body(especie);
 	}
 
 }
--- src/main/java/com/fernandes/ecommercelovepet/service/EspecieService.java
+++ src/main/java/com/fernandes/ecommercelovepet/service/EspecieService.java
@@ -1,12 +1,14 @@
 package com.fernandes.ecommercelovepet.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fernandes.ecommercelovepet.entities.Especie;
 import com.fernandes.ecommercelovepet.repository.EspecieRepository;
+import com.fernandes.ecommercelovepet.service.exception.ResourceNotFound;
 
 @Service
 public class EspecieService {
@@ -18,6 +20,12 @@ public class EspecieService {
 		List<Especie> especies = especieRepository.findAll();
 		
 		return especies;
+	}
+	
+	public Especie findById(Integer id) {
+		Optional<Especie> especie = especieRepository.findById(id);
+		
+		return especie.orElseThrow(() -> new ResourceNotFound("Especie do id:" + id + " não encontrado!"));
 	}
 
 }
