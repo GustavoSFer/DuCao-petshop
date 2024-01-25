c1c65a92d4b760c2dc77ae4ef6ad64ae78586689
GustavoSFer <gustavo.sfernander@gmail.com>
2024-01-24 21:45:46 -0300

    Feat: Criando a busca de um animal pelo id especifico

--- src/main/java/com/fernandes/ecommercelovepet/resource/AnimalResource.java
+++ src/main/java/com/fernandes/ecommercelovepet/resource/AnimalResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +26,10 @@ public class AnimalResource {
 		return ResponseEntity.ok().body(animais);
 	}
 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Animal> findById(@PathVariable Integer id) {
+		Animal animal = animalService.findById(id);
+		
+		return ResponseEntity.ok().body(animal);
+	}
 }
--- src/main/java/com/fernandes/ecommercelovepet/service/AnimalService.java
+++ src/main/java/com/fernandes/ecommercelovepet/service/AnimalService.java
@@ -1,12 +1,14 @@
 package com.fernandes.ecommercelovepet.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fernandes.ecommercelovepet.entities.Animal;
 import com.fernandes.ecommercelovepet.repository.AnimalRepository;
+import com.fernandes.ecommercelovepet.service.exception.ResourceNotFound;
 
 @Service
 public class AnimalService {
@@ -18,6 +20,12 @@ public class AnimalService {
 		List<Animal> animais = animalRepository.findAll();
 		
 		return animais;
+	}
+	
+	public Animal findById(Integer id) {
+		Optional<Animal> animal = animalRepository.findById(id);
+		
+		return animal.orElseThrow(() -> new ResourceNotFound("Animal com o id: " + id + " não encontrado."));
 	}
 
 }
