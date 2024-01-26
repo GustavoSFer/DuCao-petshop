e4911d8579ea56adbab44ed7fcf951ee152d6d34
GustavoSFer <gustavo.sfernander@gmail.com>
2024-01-25 21:20:20 -0300

    Feat: Create no controller

--- src/main/java/com/fernandes/ecommercelovepet/resource/AnimalResource.java
+++ src/main/java/com/fernandes/ecommercelovepet/resource/AnimalResource.java
@@ -1,13 +1,17 @@
 package com.fernandes.ecommercelovepet.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fernandes.ecommercelovepet.entities.Animal;
 import com.fernandes.ecommercelovepet.service.AnimalService;
@@ -31,5 +35,13 @@ public class AnimalResource {
 		Animal animal = animalService.findById(id);
 		
 		return ResponseEntity.ok().body(animal);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Animal> create(@RequestBody Animal animal) {
+		Animal createAnimal = animalService.create(animal);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(animal.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(createAnimal);
 	}
 }
