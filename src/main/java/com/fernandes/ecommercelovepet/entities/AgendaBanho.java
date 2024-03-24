a3789c5f11787623c4090a3c938beef94f37ab27
GustavoSFer <gustavo.sfernander@gmail.com>
2024-03-24 09:44:34 -0300

    Feat: Criando a classe agendaBanho que tem a chave composta

--- src/main/java/com/fernandes/ecommercelovepet/entities/AgendaBanho.java
+++ src/main/java/com/fernandes/ecommercelovepet/entities/AgendaBanho.java
@@ -1,5 +1,67 @@
 package com.fernandes.ecommercelovepet.entities;
 
-public class AgendaBanho {
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.fernandes.ecommercelovepet.entities.AgendaBanhoPK;
+
+public class AgendaBanho implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private AgendaBanhoPK id;
+	private boolean status;
+	
+	
+	public AgendaBanho(Agenda agenda, Animal animal, boolean status) {
+		super();
+		id.setAgenda(agenda);
+		id.setAnimal(animal);
+		this.status = status;
+	}
+
+	public Agenda getAgenda() {
+		return id.getAgenda();
+	}
+	
+	public void setAgenda(Agenda agenda) {
+		id.setAgenda(agenda);
+	}
+	
+	public Animal getAnimal() {
+		return id.getAnimal();
+	}
+	
+	public void setAnimal(Animal animal) {
+		id.setAnimal(animal);
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AgendaBanho other = (AgendaBanho) obj;
+		return Objects.equals(id, other.id);
+	}
 
 }
