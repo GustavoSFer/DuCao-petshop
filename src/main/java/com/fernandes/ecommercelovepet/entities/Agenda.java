c65ad530104926726fe0242aa78e798fbcbdbdf3
GustavoSFer <gustavo.sfernander@gmail.com>
2024-03-24 08:29:52 -0300

    Feal: Criando a classe de agenda

---
+++ src/main/java/com/fernandes/ecommercelovepet/entities/Agenda.java
@@ -0,0 +1,85 @@
+package com.fernandes.ecommercelovepet.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Objects;
+
+public class Agenda implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private LocalDate data;
+	private LocalTime hora;
+	private boolean status;
+	private int diaSemana;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public LocalTime getHora() {
+		return hora;
+	}
+
+	public void setHora(LocalTime hora) {
+		this.hora = hora;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public int getDiaSemana() {
+		return diaSemana;
+	}
+
+	public void setDiaSemana(int diaSemana) {
+		this.diaSemana = diaSemana;
+	}
+
+	public Agenda(LocalDate data, LocalTime hora, boolean status, int diaSemana) {
+		super();
+		this.data = data;
+		this.hora = hora;
+		this.status = status;
+		this.diaSemana = diaSemana;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Agenda other = (Agenda) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
